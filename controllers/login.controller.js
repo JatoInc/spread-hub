32fb9496db59bbb51b71e5f30b28a67e60b5bc12
Christian Moura <christianmsbrito@gmail.com>
2019-10-23 13:12:37 -0300

    authentication

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -2,12 +2,13 @@ const bcrypt = require('bcrypt');
 const crypto = require('crypto')
 const UserService = require('../services/users.service');
 const jwt = require('jsonwebtoken');
+const moment = require('moment');
 const { onSuccess } = require('../shared/helpers/finalize-request/index');
 
 class Controller {
     async login(ctx) {
         const credentials = ctx.headers.authorization.replace('Basic ', '')
-        
+
         const userAuth = Buffer.from(credentials, 'base64').toString('utf-8');
         const [email, password] = userAuth.split(':');
 
@@ -15,21 +16,25 @@ class Controller {
         if (!user || user.length == 0) {
             throw 'Email not found';
         }
-        console.log('user :', user);
 
         const authenticated = bcrypt.compareSync(password, user.password);
 
         if (!authenticated) {
             throw 'Inavlid password'
         }
-        
+
         const token = jwt.sign({
             id: user._id,
             access: user.access_level,
             refreshToken: crypto.randomBytes(20).toString('hex')
         }, process.env.JWT_SECRET, { expiresIn: 86400 });
 
-        return onSuccess(ctx, token);
+        return onSuccess(ctx, {
+            type: 'Bearer',
+            token,
+            expiresIn: 86400,
+            expireDate: new Date(moment().add(86400, 'seconds')).getTime()
+        });
     }
 }
 
--- middlewares/auth.meddleware.js
+++ middlewares/auth.meddleware.js
@@ -1,37 +1,33 @@
-const bcrypt = require('bcrypt');
-const UserService = require('../services/users.service');
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
 
+const authenticate = (ctx, next, level) => {
+    const [, token] = ctx.headers.authorization.split(' ');
+    const data = jwt.decode(token);
+    if(data.access <= level) {
+        return next()
+    }
+    ctx.status = 401
+    ctx.body = {
+        error: true,
+        message: 'Access denied: User does not have permision to access this resource!'
+    }
+}
 class Auth {
-    login(credentials) {
-        const userAuth = Buffer.from(credentials, 'base64').toString('utf8');
 
-        const [email, password] = userAuth.split(':');
-
-        const user = await UserService.find({ email });
-
-        if (!user || user.length == 0) {
-            throw 'Email not found';
-        }
-
-        const authenticated = bcrypt.compareSync(password, user.password);
-
-        if (!authenticated) {
-            throw 'Inavlid password'
-        }
-
-        const token = jwt.sign({
-            id: user._id,
-            type: user.type,
-            refreshToken: crypto.randomBytes(20).toString('hex')
-        }, process.env.JWT_SECRET, { expiresIn: 86400 });
-
-        return token
+    atLeastStudent(ctx, next) {
+        return authenticate(ctx, next, 3);
     }
 
-    refresh(token) {
-        
+    atLeastCurator(ctx, next) {
+        return authenticate(ctx, next, 2);
+    }
+
+    atLeastProfessor(ctx, next) {
+        return authenticate(ctx, next, 1);
+    }
+
+    atLeastAdmin(ctx, next) {
+        return authenticate(ctx, next, 0);
     }
 }
 
