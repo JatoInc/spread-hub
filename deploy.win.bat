49e1f3e55a1f5a409897ea6133c1df0901e8847b
Christian Moura <christianmsbrito@gmail.com>
2019-11-07 19:49:01 -0300

    deploys

--- deploy.win.bat
+++
@@ -1,6 +0,0 @@
-REM !/bin/bash
-docker service rm jatocorp_spread_hub
-docker rmi spread-hub:latest -f
-
-docker build --no-cache -t spread-hub:latest -f Dockerfile .
-docker stack deploy -c docker-compose.yml jatocorp
